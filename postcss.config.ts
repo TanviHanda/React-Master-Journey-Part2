16f406d9187e8794652311786c3e2b275b08653a
TanviHanda <tanvisona2012@gmail.com>
2025-07-28 18:32:58 +0530

    App.jsx

---
+++ postcss.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // Add other PostCSS plugins here as needed
+  },
+};
+
+export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@radix-ui/themes/styles.css";
 import App from "./App";
+import { Theme } from "@radix-ui/themes";
 // import MyProvider from "./components/MyContext";
 import "./index.css"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <MyProvider> */}
+    <Theme>
       <App />
     {/* </MyProvider> */}
+    </Theme>
   </StrictMode>
 );
